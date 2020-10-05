a7abb0b5829108a31c35dbd57e77874313659ebf
Maria Alejandra Robayo Sepúlveda <marobayos@unal.edu.co>
2020-10-05 01:20:22 -0500

    :sparkles: Adding mouse events

--- _sketches/tresshold.js
+++ _sketches/tresshold.js
@@ -1,5 +1,5 @@
 var tresshold = 70
-var img, canvas
+var img, canvas, index;
 function preload(){
     img = loadImage("data/download.jpg");
 }
@@ -7,28 +7,16 @@ function preload(){
 function setup() {
     canvas = createCanvas(400, 400);
     canvas.parent('sketch-div');
-    image(img,0,0);
-    loadPixels();
-    for (let i = 0; i < pixels.length; i+=4) {
-        let c = color(pixels[i], pixels[i + 1], pixels[i + 2])
-        if (brightness(c) < tresshold) {
-            pixels[i] = pixels[i + 1] = pixels[i + 2] = 0;
-        } else {
-            pixels[i] = pixels[i + 1] = pixels[i + 2] = 255;
-        }
-    }
-    updatePixels();
+    frameRate(30);
 }
 
 function draw() {
     image(img,0,0);
     loadPixels();
-    let d = pixelDensity();
-    console.log(d)
-    for (let i = 0; i < 300; i++) {
-        for (let j = 0; j < Math.max(Math.min(mouseX, 300), 0); j++) {
+    for (let i = 0; i < 400; i++) {
+        for (let j = 0; j < Math.max(Math.min(mouseX, 400), 0); j++) {
             // loop over
-            index = 4 * ((j) * width *  + (i));
+            index = 4 * ((j) * 400 *  + (i));
             if (brightness(color(pixels[index],pixels[index + 1],pixels[index + 2])) < tresshold) {
                 pixels[index] = pixels[index + 1] = pixels[index + 2] = 0;
             } else {
@@ -36,6 +24,6 @@ function draw() {
             }
         }
     }
-    console.log(Math.max(Math.min(mouseX, 300), 0))
+    console.log(Math.max(Math.min(mouseX, 400), 0))
     updatePixels();
 }
