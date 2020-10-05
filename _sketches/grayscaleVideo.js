37175b7b8a3b51200557aa2ab92d31ca291c9d92
nfgavilans <nfgavialns@unal.edu.co>
2020-10-05 03:54:36 -0500

    Videos

--- _sketches/grayscaleVideo.js
+++ _sketches/grayscaleVideo.js
@@ -7,14 +7,14 @@ function preload() {
 }
 
 function setup() {
-    let canvas = createCanvas(400, 400);
+    let canvas = createCanvas(600, 600);
     canvas.parent('sketch-div');
 }
 
 function draw() {
     image(vid, 0, 0);
     filter(GRAY);
-    textSize(20);
+    textSize(16);
     text("Frame Rate= " + frameRate().toFixed(3), 100, 30);
     text("Frame Count = " + frameCount, 100, 70);
     text("Delta time between previous and current frame = ", deltaTime.toFixed(3), 100, 110);
