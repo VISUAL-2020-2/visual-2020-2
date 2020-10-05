89f799b4098b684872ca18c096618d92e1042045
nfgavilans <nfgavialns@unal.edu.co>
2020-10-05 01:57:03 -0500

    Show video

--- _sketches/grayscaleVideo.js
+++ _sketches/grayscaleVideo.js
@@ -6,6 +6,18 @@ function setup() {
     vid = createVideo(['data/video-sample.webm']);
     vid.hide();
     vid.loop();
+    vid.loadPixels();
+    for (let j = 0; j < 400; j++) {
+        for (let i = 0; i < Math.max(Math.min(mouseX, 400), 0); i++) {
+            index = 4 * ((j) * 400 + (i));
+            if (brightness(color(pixels[index], pixels[index + 1], pixels[index + 2])) < tresshold) {
+                pixels[index] = pixels[index + 1] = pixels[index + 2] = 0;
+            } else {
+                pixels[index] = pixels[index + 1] = pixels[index + 2] = 255;
+            }
+
+        }
+    }
 }
 
 function draw() {
