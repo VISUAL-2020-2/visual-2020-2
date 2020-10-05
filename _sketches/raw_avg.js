e787e6bdf0c7b829681ba8544053d5e2301fbed7
Maria Alejandra Robayo Sepúlveda <marobayos@unal.edu.co>
2020-10-05 00:32:09 -0500

    :sparkles: Adding raw avg grayscale

--- _sketches/raw_avg.js
+++ _sketches/raw_avg.js
@@ -1,4 +1,3 @@
-var tresshold = 70
 var img
 function preload(){
     img = loadImage("data/download.jpg");
@@ -10,12 +9,10 @@ function setup() {
     image(img,0,0);
     loadPixels();
     for (let i = 0; i < pixels.length; i+=4) {
-        let c = color(pixels[i], pixels[i + 1], pixels[i + 2])
-        if (brightness(c) < tresshold) {
-            pixels[i] = pixels[i + 1] = pixels[i + 2] = 0;
-        } else {
-            pixels[i] = pixels[i + 1] = pixels[i + 2] = 255;
-        }
+        let c = color((pixels[i] + pixels[i + 1] +  pixels[i + 2])/3);
+        pixels[i] = red(c);
+        pixels[i + 1] = green(c);
+        pixels[i + 2] = blue(c);
     }
     updatePixels();
 }
---
+++ _sketches/raw_avg.md
@@ -0,0 +1,10 @@
+---
+title: Grayscale RAW Average
+---
+
+Here we have a gray scale filter using the average of the RAW values for each pixel:
+
+<!-- Sketch file location, (pending organization) -->
+<script src="raw_avg.js"></script>
+<!-- Necessary element to position p5 canvas -->
+<div id="sketch-div"></div>
