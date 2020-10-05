2b18523f69c956c852fbf9b849ea441194787bc8
Maria Alejandra Robayo Sepúlveda <marobayos@unal.edu.co>
2020-10-05 03:28:22 -0500

    :bug: Fixing video features

--- _sketches/grayscaleVideo.js
+++ _sketches/grayscaleVideo.js
@@ -2,14 +2,20 @@ var vid, tresshold = 50, capture;
 
 function preload() {
     vid = createVideo('data/video-sample.webm', vidLoad);
+    vid.hide();
 }
 
 function setup() {
+    createCanvas(400, 400)
     vid.parent('sketch-div');
+}
+
+function draw(){
+    image(vid, 0, 0);
+    filter(THRESHOLD);
 }
 
 function vidLoad(){
     vid.loop();
     vid.volume(0);
-    filter(THRESHOLD);
 }
