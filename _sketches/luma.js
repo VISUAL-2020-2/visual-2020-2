f34d836fec4fb1062f3a01fd122b0cfd7569eedb
jcvar <36166613+jcvar@users.noreply.github.com>
2020-10-05 08:42:03 -0500

    Bring changes from raw_avg.js

--- _sketches/luma.js
+++ _sketches/luma.js
@@ -1,25 +1,27 @@
-var img
-function preload(){
+var img;
+function preload() {
     img = loadImage("data/descarga.jpg");
 }
 
 function setup() {
     var canvas = createCanvas(400, 400);
     canvas.parent('sketch-div');
+    textAlign(LEFT, TOP);
+    fill(255);
 }
 
-function draw(){
-    image(img,0,0);
+function draw() {
+    image(img, 0, 0);
     loadPixels();
-    text(frameRate(), 10, 10);
     for (let j = 0; j < 400; j++) {
         for (let i = 0; i < Math.max(Math.min(mouseX, 400), 0); i++) {
             index = 4 * ((j) * 400 + (i));
-            let c = color((0.2126*pixels[index] + 0.0722*pixels[index + 1] +  0.7152*pixels[index + 2])/3);
-            pixels[index] = red(c);
-            pixels[index + 1] = green(c);
-            pixels[index + 2] = blue(c);
+            let c = (0.2126 * pixels[index] + 0.0722 * pixels[index + 1] + 0.7152 * pixels[index + 2]) / 3;
+            pixels[index] = c;
+            pixels[index + 1] = c;
+            pixels[index + 2] = c;
         }
     }
     updatePixels();
+    text(frameRate().toFixed(3), 0, 0);
 }
