eb6d83e599537ea50e2f56b99db5a591a5d5f885
jcvar <36166613+jcvar@users.noreply.github.com>
2020-10-05 08:28:51 -0500

    Fix framerate text()

--- _sketches/raw_avg.js
+++ _sketches/raw_avg.js
@@ -1,21 +1,22 @@
 var img
-function preload(){
+function preload() {
     img = loadImage("data/descarga.jpg");
 }
 
 function setup() {
     var canvas = createCanvas(400, 400);
     canvas.parent('sketch-div');
+    textAlign(LEFT, TOP);
+    fill(255);
 }
 
-function draw(){
-    image(img,0,0);
+function draw() {
+    image(img, 0, 0);
     loadPixels();
-    text(frameRate(), 10, 10);
     for (let j = 0; j < 400; j++) {
         for (let i = 0; i < Math.max(Math.min(mouseX, 400), 0); i++) {
             index = 4 * ((j) * 400 + (i));
-            let c = color((pixels[index] + pixels[index + 1] +  pixels[index + 2])/3);
+            let c = color((pixels[index] + pixels[index + 1] + pixels[index + 2]) / 3);
             pixels[index] = red(c);
             pixels[index + 1] = green(c);
             pixels[index + 2] = blue(c);
@@ -23,4 +24,5 @@ function draw(){
         }
     }
     updatePixels();
+    text(frameRate().toFixed(3), 0, 0); // text AFTER image
 }
