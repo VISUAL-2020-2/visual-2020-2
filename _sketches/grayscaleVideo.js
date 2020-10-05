8c9e108b2532f4544d92d82ba8d2993e6f861a25
nfgavilans <nfgavialns@unal.edu.co>
2020-10-05 01:35:06 -0500

    Show video

--- _sketches/grayscaleVideo.js
+++ _sketches/grayscaleVideo.js
@@ -3,14 +3,14 @@ let vid;
 function setup() {
     createCanvas(400, 400);
     // specify multiple formats for different browsers
-    fingers = createVideo(['data/video-sample.webm']);
-    fingers.hide(); // by default video shows up in separate dom
-    fingers.loop();
+    vid = createVideo(['data/video-sample.webm']);
+    vid.hide(); // by default video shows up in separate dom
+    vid.loop();
     // element. hide it and draw it to the canvas
     // instead
 }
 
 function draw() {
     background(150);
-    image(fingers, 10, 10); // draw the video frame to canvas
+    image(div, 10, 10); // draw the video frame to canvas
 }
