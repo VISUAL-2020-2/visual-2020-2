3793ac949f4d8e2ca94d0e2321bdbec3699943cc
María Alejandra Robayo Sepúlveda <marobayos@unal.edu.co>
2020-10-05 09:58:22 -0500

    Update thresholdvideo.js

--- _sketches/thresholdvideo.js
+++ _sketches/thresholdvideo.js
@@ -8,11 +8,14 @@ function preload() {
 function setup() {
     let canvas = createCanvas(400, 400);
     canvas.parent('sketch-div');
+    textAlign(LEFT, TOP);
+    fill(255);
 }
 
 function draw(){
     image(vid, 0, 0);
     filter(THRESHOLD);
+    text(frameRate().toFixed(3), 0, 0);
 }
 
 function vidLoad(){
