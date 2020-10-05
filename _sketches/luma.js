bc766ef821d406832b2b2c94f23c5c47dfdcfa29
Maria Alejandra Robayo Sepúlveda <marobayos@unal.edu.co>
2020-10-05 01:31:36 -0500

    :sparkles: Adding mouse events

--- _sketches/luma.js
+++ _sketches/luma.js
@@ -11,7 +11,7 @@ function setup() {
     for (let j = 0; j < 400; j++) {
         for (let i = 0; i < Math.max(Math.min(mouseX, 400), 0); i++) {
             index = 4 * ((j) * 400 + (i));
-            let c = color((0.2126*pixels[i] + 0.0722*pixels[i + 1] +  0.7152*pixels[i + 2])/3);
+            let c = color((0.2126*pixels[index] + 0.0722*pixels[index + 1] +  0.7152*pixels[index + 2])/3);
             pixels[index] = red(c);
             pixels[index + 1] = green(c);
             pixels[index + 2] = blue(c);
--- _sketches/raw_avg.js
+++ _sketches/raw_avg.js
@@ -11,7 +11,7 @@ function setup() {
     for (let j = 0; j < 400; j++) {
         for (let i = 0; i < Math.max(Math.min(mouseX, 400), 0); i++) {
             index = 4 * ((j) * 400 + (i));
-            let c = color((pixels[i] + pixels[i + 1] +  pixels[i + 2])/3);
+            let c = color((pixels[index] + pixels[index + 1] +  pixels[index + 2])/3);
             pixels[index] = red(c);
             pixels[index + 1] = green(c);
             pixels[index + 2] = blue(c);
