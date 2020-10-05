0f1e61242ba8c757ee276821756e6e206f586be2
Maria Alejandra Robayo Sepúlveda <marobayos@unal.edu.co>
2020-10-05 00:13:33 -0500

    :bug: hotfix

--- _sketches/tresshold.js
+++ _sketches/tresshold.js
@@ -1,7 +1,9 @@
-var tresshold = 128
+var tresshold = 127
 var img
 function preload(){
     img = loadImage("data/download.jpg");
+    let inp = createInput('thresshold');
+    inp.input(reload());
 }
 
 function setup() {
@@ -10,8 +12,24 @@ function setup() {
     image(img,0,0);
     loadPixels();
     for (let i = 0; i < pixels.length; i+=4) {
-        let c = color(pixels[i], pixels[i + 1], pixels[i + 2], pixels[i + 3])
+        let c = color(pixels[i], pixels[i + 1], pixels[i + 2])
         if (brightness(c) < tresshold) {
+            pixels[i] = pixels[i + 1] = pixels[i + 2] = 0;
+        } else {
+            pixels[i] = pixels[i + 1] = pixels[i + 2] = 255;
+        }
+    }
+    updatePixels();
+}
+
+function reload() {
+    var canvas = createCanvas(400, 400);
+    canvas.parent('sketch-div');
+    image(img,0,0);
+    loadPixels();
+    for (let i = 0; i < pixels.length; i+=4) {
+        let c = color(pixels[i], pixels[i + 1], pixels[i + 2])
+        if (brightness(c) < this.value()) {
             pixels[i] = pixels[i + 1] = pixels[i + 2] = 0;
         } else {
             pixels[i] = pixels[i + 1] = pixels[i + 2] = 255;
