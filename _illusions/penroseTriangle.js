e886b6de09f8c59853d8e703e5894066b3880630
Maria Alejandra Robayo Sepúlveda <marobayos@unal.edu.co>
2020-11-04 01:27:01 -0500

    :sparkles: Adding 3d illusion

--- _illusions/penroseTriangle.js
+++ _illusions/penroseTriangle.js
@@ -2,6 +2,10 @@
 function setup() {
     var canvas = createCanvas(400, 400, WEBGL); // Keep canvas reference
     canvas.parent('sketch-div'); // Necessary to position canvas
+
+}
+
+function draw() {
     beginShape();
     fill(237, 34, 93);
     vertex(0, 0, 0);
@@ -13,11 +17,7 @@ function setup() {
     vertex(300, 100, 100);
     vertex(300, 100, 0);
     endShape(CLOSE);
-}
-
-function draw() {
-    rotateX(frameCount);
-    rotateZ(frameCount);
-    rotateY(frameCount);
+    rotateX(frameCount * 0.01);
+    rotateY(frameCount * 0.01);
 }
 
