f21aee4a8d25c278d3a914f3df18eb2c57b2c4b8
diarodriguezmo <diarodriguezmo@unal.edu.co>
2020-11-24 20:42:27 -0500

    light attenuation fixed

--- _light/atte.js
+++ _light/atte.js
@@ -1,5 +1,6 @@
 let piramide;
 let pos=1.57*1.70;
+let zPositionValue=0
 function preload() {
   piramide = loadModel('assets/pir.obj', true);
 }
@@ -7,9 +8,6 @@ function preload() {
 function setup() {
   var canvas = createCanvas(400, 400, WEBGL);
   canvas.parent('sketch-div');
-  zPosSlider = createSlider(-200, 0, 0, 0.1);
-  zPosSlider.parent('sketch-div');	 
-  zPosSlider.position(20, 80);
 }
 
 function draw() {
@@ -18,11 +16,26 @@ function draw() {
   xPositionValue = (mouseX - width / 2)*2;
   yPositionValue = (mouseY - height / 2)*2;
   noStroke()
-  zPositionValue = zPosSlider.value()
   ambientLight(0)
   lightFalloff(1, 0.01,0);
-  pointLight(255, 255, 255, xPositionValue, yPositionValue, -zPositionValue);
+  pointLight(255, 255, 255, xPositionValue, yPositionValue,zPositionValue);
   rotateX(pos)
   specularMaterial(255, 255, 0)
   model(piramide)
+}
+
+function keyPressed() {
+  if (keyCode === DOWN_ARROW) {
+    if (zPositionValue >= 1000){
+      zPositionValue = zPositionValue;
+    } else{
+      zPositionValue = zPositionValue + 100;
+    }
+  } else if (keyCode === UP_ARROW) {
+    if (zPositionValue <= 0){
+      zPositionValue = zPositionValue;
+    } else{
+      zPositionValue = zPositionValue - 100;
+    }
+  }
 }
--- _light/atte.md
+++ _light/atte.md
@@ -2,7 +2,7 @@
 title: Light Attenuation
 ---
 
-This is the light attenuation sketch, you can play with the light's position by moving your mouse all over the canvas, or changing it's depth with the slider on top. This example also uses specular light.
+This is the light attenuation sketch, you can play with the light's position by moving your mouse all over the canvas, or changing it's depth with the up and down arrows. This example also uses specular light.
 
 <!-- Sketch file location, (pending organization) -->
 <script src="atte.js"></script>
