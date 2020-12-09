fb9d2482965f67b1cd39c3b1fae0750329dabfa6
Maria Alejandra Robayo Sepúlveda <marobayos@unal.edu.co>
2020-12-09 00:49:16 -0500

    :loud_sound: Adding logs

--- _project/project.js
+++ _project/project.js
@@ -21,7 +21,6 @@ function setup() {
   img.resize(400, 400);
   flippedVideo = ml5.flipImage(img);
   // Start classifying
-  classifyVideo();
   var input = createFileInput(handleFile);
   input.parent('histogram-div');
 }
@@ -53,6 +52,7 @@ function gotResult(error, results) {
   label = results[0].label;
   classifyVideo();
 }
+
 function handleFile(file) {
   if (file.type === 'image') {
     img = loadImage(file.data);
